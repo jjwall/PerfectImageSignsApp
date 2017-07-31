cb51058c509230528736136911a26a36a05665d5
Jacob Wallace <jacob.wallace@utexas.edu>
2017-07-31 17:35:58 -0500

    more clean up on postsign.js

--- public/javascript/postsign.js
+++ public/javascript/postsign.js
@@ -1,33 +1,8 @@
 $(document).ready(function(){
 
-	/*! modernizr 3.5.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-setclasses !*/
-!function(n,e,s){function o(n,e){return typeof n===e}function a(){var n,e,s,a,i,l,r;for(var c in f)if(f.hasOwnProperty(c)){if(n=[],e=f[c],e.name&&(n.push(e.name.toLowerCase()),e.options&&e.options.aliases&&e.options.aliases.length))for(s=0;s<e.options.aliases.length;s++)n.push(e.options.aliases[s].toLowerCase());for(a=o(e.fn,"function")?e.fn():e.fn,i=0;i<n.length;i++)l=n[i],r=l.split("."),1===r.length?Modernizr[r[0]]=a:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=a),t.push((a?"":"no-")+r.join("-"))}}function i(n){var e=r.className,s=Modernizr._config.classPrefix||"";if(c&&(e=e.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+s+"no-js(\\s|$)");e=e.replace(o,"$1"+s+"js$2")}Modernizr._config.enableClasses&&(e+=" "+s+n.join(" "+s),c?r.className.baseVal=e:r.className=e)}var t=[],f=[],l={_version:"3.5.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(n,e){var s=this;setTimeout(function(){e(s[n])},0)},addTest:function(n,e,s){f.push({name:n,fn:e,options:s})},addAsyncTest:function(n){f.push({name:null,fn:n})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var r=e.documentElement,c="svg"===r.nodeName.toLowerCase();a(),i(t),delete l.addTest,delete l.addAsyncTest;for(var u=0;u<Modernizr._q.length;u++)Modernizr._q[u]();n.Modernizr=Modernizr}(window,document);
-
-	// function handlePermission() {
- //  		navigator.permissions.query({name:'geolocation'}).then(function(result) {
- //    		if (result.state == 'granted') {
- //     			report(result.state);
- //      			//geoBtn.style.display = 'none';
- //    		} else if (result.state == 'prompt') {
- //      			report(result.state);
- //      			//geoBtn.style.display = 'none';
- //      			navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
- //    		} else if (result.state == 'denied') {
- //      			report(result.state);
- //      			//geoBtn.style.display = 'inline';
- //    		}
- //    		result.onchange = function() {
- //      			report(result.state);
- //    		}
- //  		});
-	// }
-
-	// function report(state) {
- //  		console.log('Permission ' + state);
-	// }
-
-	// handlePermission();
+	// navigator.permissions.query({name:'geolocation'}).then(function(result){
+	// 	alert("permission state: " + result.state);
+	// })
 
 	var companyName = $("#company-name");
 
@@ -61,19 +36,6 @@ $(document).ready(function(){
 		}
 	}
 
-	// test code:
-	// function getLoc(){
-	// 	while (position === undefined)
-	// 	navigator.geolocation.getCurrentPosition(showPos);
-	// }
-	//
-	// function showPos(position) {
-	// 	getLoc();
-	// 	console.log(position);
-	// }
-	//getLoc();
-	// end test code
-
 	// lot going into the submitPost function, we use getLocation and showPosition
 	// to utilize the HTML5 Geolocation API, we capture data that was inputed for
 	// company name and description of sign on the client-side and we use moment.js
@@ -95,7 +57,7 @@ $(document).ready(function(){
 		function geo_error() {
 			// when geo_error fires, we still capture company name and description, but we do not automatically
 			// have access to user's lat/lon coordinates. So we pop up a module asking to input lat/lon coordinates manually
-  		alert("Either your device or browser does not support Geolocation. Please input your long/lat manually:");
+  		alert("Either your device and/or browser does not support Geolocation. Please input your long/lat coords manually:");
 			$("#submit-post").removeClass("is-loading");
 		}
 
@@ -118,8 +80,6 @@ $(document).ready(function(){
 					$("#submit-post").removeClass("is-loading");
 					alert(request.responseText);
 				}
-				//}
-				//console.log(newPost);
 			}).done(function(data) {
 				companyName.val("");
 				description.val("");
@@ -131,9 +91,6 @@ $(document).ready(function(){
 				$("#submitPostError").empty();
 				$("#submitPostError").append("<div><p>Post successfully submitted!</p></div>");
 			});
-			//} // end success function
-			//});
 		}
-		// showPosition();
 	}
 });
